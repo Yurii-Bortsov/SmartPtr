931a90bbcac04593c6a2fff6383eef5a28473ac1
Yurii-Bortsov <bortsov.iu@yandex.ru>
2023-12-20 22:06:17 +0300

    все работает

---
+++ student.h
@@ -0,0 +1,25 @@
+#include <memory>
+#include <string>
+
+class Classroom;
+
+class Student : public std::enable_shared_from_this<Student>
+{
+private:
+    std::string name;
+    const int age;
+    std::shared_ptr<Classroom> myClassroom;
+public:
+    Student(const std::string& name, int age);
+
+    /** 
+     * @brief Определяет студенту аудиторию.
+     * @param classroom Указатель shared_ptr на аудиторию, к которой присоединяется студент.
+    */
+    void currentClassroom(const std::shared_ptr<Classroom>& classroom);
+
+    /** 
+     * @brief Отображает информацию о студенте.
+     */
+    void displayInfo();
+};
---
+++ teacher.h
@@ -0,0 +1,27 @@
+#include <memory>
+#include <string>
+
+class Classroom;
+
+class Teacher : public std::enable_shared_from_this<Teacher>
+{
+private:
+    std::string name;
+    std::weak_ptr<Classroom> myClassroom;
+public:
+    /**
+     * @brief Конструктор класса учитель.
+    */
+    Teacher(const std::string& name);
+
+    /** 
+     * @brief Присоединяет учителя к аудитории.
+     * @param classroom Указатель shared_ptr на аудиторию, к которой присоединяется учитель.
+     */
+    void currentClassroom(const std::shared_ptr<Classroom>& classroom);
+
+    /** 
+     * @brief Отображает информацию о учителе.
+     */
+    void displayInfo();
+};
