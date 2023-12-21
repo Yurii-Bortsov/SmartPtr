75e04c4a711ae6381c1027f55b0bd8c6aec41b8c
Юрий Борцов <113592883+Yurii-Bortsov@users.noreply.github.com>
2023-12-21 14:25:09 +0300

    cout -> streamBuffer

--- classroom.cpp
+++ classroom.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "classroom.h"
 #include "student.h"
 #include "teacher.h"
@@ -13,23 +14,25 @@ void Classroom::addTeacher(const std::shared_ptr<Teacher>& teacher)
     this->teacher = teacher;
 }
 
-void Classroom::displayClassInfo()
+std::string Classroom::displayClassInfo()
 {
-    std::cout << "Class Info:\n";
+    std::ostringstream streamBuffer;
+    streamBuffer << "Class Info:\n";
     if (teacher != nullptr)
     {
-        std::cout << "\n";
-        teacher->displayInfo();
+        streamBuffer << "\n";
+        streamBuffer << teacher->displayInfo();
     }
-    std::cout << "Students:";
-    std::cout << "\n";
+    streamBuffer << "Students:";
+    streamBuffer << "\n";
 
     for (std::vector<std::weak_ptr<Student>>::iterator temp = students.begin(); temp != students.end(); ++temp)
     {
         if (std::shared_ptr<Student> sharedStudent = temp->lock())
         {
-            sharedStudent->displayInfo();
-            std::cout << "\n";
+            streamBuffer << sharedStudent->displayInfo();
+            streamBuffer << "\n";
         }
     }
+    return streamBuffer.str();
 }
